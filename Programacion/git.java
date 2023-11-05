4079d1053208d458b77e827ed21bf964724dad96
Luis Alberto <luisalbertolaramartinez3c@gmail.com>
2023-11-05 19:07:45 +0100

    Modificacion del archivo .java de la carpeta Programacion

--- Programacion/git.java
+++ Programacion/git.java
@@ -1,5 +1,6 @@
 public class git{
 	public static void main (String[]args){
 		System.out.println("Este archivo es usado para git");
+		System.out.println("Modificación que se añade al repositorio local");
 	}
 }
